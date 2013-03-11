67887b049fc01acbe4eae3cc9978d8670e2df1f8
Shaun Smith <disgone@gmail.com>
2013-03-11 16:21:09 -0500

    Added RSVP actions to register and cancel registration.

--- src/MassScheduler/MassScheduler/Controllers/RSVPController.cs
+++ src/MassScheduler/MassScheduler/Controllers/RSVPController.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using MassScheduler.Models;
 
 namespace MassScheduler.Controllers
 {
+    [Authorize]
     public class RSVPController : ControllerBase
     {
-        //
-        // GET: /RSVP/
-
-        public ActionResult Index()
+        public ActionResult Register(int id)
         {
-            return View();
+            var meeting = db.Meetings.Find(id);
+
+            if (meeting == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (!meeting.IsUserAttending(CurrentUser.Username))
+            {
+                var reservation = new RSVP(CurrentUser.Username, CurrentUser.FullName, CurrentUser.EmailAddress);
+                meeting.RSVP.Add(reservation);
+                db.SaveChanges();
+            }
+
+            return View(meeting);
         }
 
+        public ActionResult Cancel(int id)
+        {
+            var meeting = db.Meetings.Find(id);
+
+            var rsvp = meeting.RSVP.SingleOrDefault(x => x.AttendeeUsername == CurrentUser.Username);
+            if (rsvp != null)
+            {
+                db.RSVPs.Remove(rsvp);
+                db.SaveChanges();
+            }
+
+            return View(meeting);
+        }
     }
 }
