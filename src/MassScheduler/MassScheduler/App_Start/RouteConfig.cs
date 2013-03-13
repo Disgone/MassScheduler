55d645c259a857666f1e3445fc7dd4de6fa3bca1
Shaun Smith <disgone@gmail.com>
2013-03-13 14:54:15 -0500

    Removed home controller and changed default route to the meeting schedule.

--- src/MassScheduler/MassScheduler/App_Start/RouteConfig.cs
+++ src/MassScheduler/MassScheduler/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace MassScheduler
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "meetings", action = "index", id = UrlParameter.Optional }
             );
         }
     }
